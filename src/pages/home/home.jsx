b50bbc4a9a6a08f69c8078bd0760947cdd90fa28
digocoutinho78 <digocoutinho@outlook.com>
2024-06-05 17:01:58 -0300

    inicial grid home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,33 +1,38 @@
 import React from "react";
 import Navegaçao from "../../nav";
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import Accessibility from "../../components/accessibility/accessibility";
-import { useTranslation } from 'react-i18next'
-
-
-
+import { useTranslation } from "react-i18next";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 
 export default function Home() {
+  const { t } = useTranslation();
 
-    const {t} = useTranslation();
+  console.log(t("kkk"));
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <Navegaçao />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          {/* Colocar o Accessibility dentro do HEADER */}
+          {/* <Accessibility/>  */}
 
-    console.log(t('kkk'))
-    return (
-        <>
-       
-            <Box sx={{ display: 'flex' }}>
-                <Navegaçao />
-                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                <h1>{t('Home')}</h1>
-                    {/* <Typography paragraph>
+          <Grid container spacing={2}>
+            <Grid xs={12} style={{ backgroundColor: "#2D939C" }}></Grid>
+            <Grid xs="auto" style={{ backgroundColor: "#2D519C" }}></Grid>
+            <Grid xs={8} style={{ backgroundColor: "#2D617D" }}>
+              <h1>{t("Home")}</h1>
+            </Grid>
+            <Grid xs style={{ backgroundColor: "#2D720C" }}></Grid>
+            <Grid xs={12} style={{ backgroundColor: "#2D939C" }}></Grid>
+          </Grid>
+
+          {/* <Typography paragraph>
           -------------Espaço para texto-------------
         </Typography> */}
-
-                </Box>
-            </Box>
-
-        </>
-
-    )
+        </Box>
+      </Box>
+    </>
+  );
 }
